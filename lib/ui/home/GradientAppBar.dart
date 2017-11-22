438cf7c4068dbb99d9a6515ce6f4b7e92322c000
Niamh Power <niamhpower@live.com>
2017-11-22 15:27:20 +0000

    implemented gradient for app bar

--- lib/ui/home/GradientAppBar.dart
+++ lib/ui/home/GradientAppBar.dart
@@ -15,7 +15,18 @@ class GradientAppBar extends StatelessWidget {
     return new Container(
       padding: new EdgeInsets.only(top: statusBarHeight),
       height: barHeight + statusBarHeight,
-      decoration: new BoxDecoration(color: Colors.blue),
+      decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+              colors: [
+                const Color(0xFF3366FF),
+                const Color(0xFF00CCFF),
+              ],
+              begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp
+      ),
+    ),
       child: new Center(
         child: new Text(
           title,
